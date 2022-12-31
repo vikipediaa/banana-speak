f2ad665ff9c37ad0bb6c96622d551001cacea98e
Vikas Pandey <pandey222vikas@gmail.com>
2022-12-31 20:37:19 +0530

    Queryseltion done in js

---
+++ app.js
@@ -0,0 +1,6 @@
+var  btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "";
+
