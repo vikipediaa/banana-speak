9824f61f15146243ff978a02533d9a642bb0f6f6
Vikas Pandey <pandey222vikas@gmail.com>
2023-01-03 11:53:00 +0530

    Server  part done

--- app.js
+++ app.js
@@ -2,5 +2,27 @@ var  btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+function getTranslationURL(input)
+{
+    return serverURL + "?" + "text="  + input
+};
+function errorHandler(error){
+    // console.log("error occured",console.error);
+    alert("something went worng");
+}
 
+function clickHandler()
+{
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var tralatedText = json.contents.translated;
+        outputDiv.innerText = tralatedText;
+    }
+
+    )
+    .catch(errorHandler)
+};
+btnTranslate.addEventListener("click",clickHandler)
